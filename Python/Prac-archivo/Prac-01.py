525086f292e5fe5d1fc3ce4445dd582dbe0824f6
KevinTimaran <kevin.buesaquillo@campusucc.edu.co>
2026-01-08 17:30:13 -0500

    practica en md 1

---
+++ Python/Prac-archivo/Prac-01.py
@@ -0,0 +1,29 @@
+verdadedo = "Seleccionado"
+falso = "no fue seleccionado"
+
+
+def darNombre (pregunta):
+    pregunta = input ("Cual es su nombre: ")
+    return pregunta
+
+
+
+opcion = input ("Seleccione una opcion valida:"
+    "1 si desea continuar"
+    "2 si desea terminar"
+    "Digite su opción:  ")
+
+
+
+lista = [darNombre]
+
+
+
+if opcion == "1":
+    print (darNombre( ))
+if opcion == "2":
+    print ("Gracias :) ")
+else:
+    print ("Por favor seleccione una opcion valida")
+
+
