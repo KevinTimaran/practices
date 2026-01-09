47d296bc0d6cbd76e7980094bda0bb5f6e54aa85
KevinTimaran <kevin.buesaquillo@campusucc.edu.co>
2026-01-09 17:42:05 -0500

    09/01/2026

--- Python/Prac-archivo/Prac-01.py
+++ Python/Prac-archivo/Prac-01.py
@@ -1,29 +1,55 @@
-verdadedo = "Seleccionado"
-falso = "no fue seleccionado"
+"""Problema a desarrollar:
+
+El módulo debe permitir registrar el nombre de un estudiante y tres calificaciones numéricas.
+A partir de esos datos, el programa debe calcular el promedio de las calificaciones y determinar
+el resultado final del estudiante según las siguientes reglas:
+
+Si el promedio es mayor o igual a 3.0, el estudiante aprueba.
+
+Si el promedio es menor a 3.0, el estudiante reprueba.
+
+El módulo debe mostrar:
+
+El nombre del estudiante.
+
+El promedio final calculado.
+
+El estado final (aprueba o reprueba)."""
 
 
-def darNombre (pregunta):
-    pregunta = input ("Cual es su nombre: ")
-    return pregunta
+def darNombre ():
+    return darNombre()
+
+def calcula_nota ():
+    pass
+
+def darPregunta ():
+    name_student= str(input("Hola por favor digite el nombre del estudiante: "))
+
+    note_1 = float(input ("Digite primera nota: "))
+    note_2 = float(input ("Digite segunda nota: "))
+    note_3 = float(input ("Digite Tercera nota: "))
+
+    if name_student == str:
+        return note_1
+    if name_student != str:
+        return (f"Por favor el nombre deben ser letras y no numeros {name_student}")
+    if note_1 == int:
+        return note_2
+    if note_1 != int:
+        return (f"Por favor la nota deben ser valores numericos {note_1}")
+    if note_2 == int:
+        return note_3
+    if note_2 != int:
+        return (f"Por favor la nota deben ser valores numericos {note_2}")
+    if note_3 == int:
+        return calcula_nota()
+    else:
+        return (f"Por favor la nota deben ser valores numericos {note_3}")
+    
+
+def darRespuesta ():
+    pass
 
 
-
-opcion = input ("Seleccione una opcion valida:"
-    "1 si desea continuar"
-    "2 si desea terminar"
-    "Digite su opción:  ")
-
-
-
-lista = [darNombre]
-
-
-
-if opcion == "1":
-    print (darNombre( ))
-if opcion == "2":
-    print ("Gracias :) ")
-else:
-    print ("Por favor seleccione una opcion valida")
-
-
+print (darPregunta())
