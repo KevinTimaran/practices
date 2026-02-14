5ff31ed5248bc37e7c0c5852dc4840e6a57cf487
Kevin Timaran <kevin.buesaquillo@campusucc.edu.co>
2026-02-13 23:35:38 -0500

    13/02/2025

--- Java/src/tipos_de_referencia.java
+++ Java/src/tipos_de_referencia.java
@@ -1,6 +1,12 @@
 public class tipos_de_referencia {
     public static void main(String[] args) {
         String text = "Hola mundo";
+
+        int largo = text.length();
+
+        
+
+
     } 
 
 }
