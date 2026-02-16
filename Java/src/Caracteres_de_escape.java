a199c757bf7486df28e2b66cd4ff6405c72ff7d4
KevinTimaran <kevin.buesaquillo@campusucc.edu.co>
2026-02-16 12:53:54 -0500

    16/02/2026

---
+++ Java/src/Caracteres_de_escape.java
@@ -0,0 +1,6 @@
+public class Caracteres_de_escape {
+    public static void main(String[] args) {
+        String texto = "C: \\Hola \" mundo\">";
+        System.out.println(texto);
+    }
+}
--- Java/src/tipos_primitivos.java
+++ Java/src/tipos_primitivos.java
@@ -16,6 +16,7 @@ public class tipos_primitivos {
 
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println(isActive);
         System.out.println(hoursNumber);
 
     }
