f124566051fb92636552bc6e6b834a1567b9915e
KevinTimaran <kevin.buesaquillo@campusucc.edu.co>
2026-02-20 12:18:00 -0500

    Configura gitignore y agrega nuevas clases

--- Java/src/arrays.java
+++ Java/src/arrays.java
@@ -2,14 +2,34 @@ import java.util.Arrays;
 
 public class arrays {
     public static void main(String[] args) {
-        int[] numeros = new int[10];
+        int[] numeros = new int[10]; // Se crea y se selecciona la cantidad de items que debe tener el array
+        int[] numbers = {2,1,32,4,3,2,3,4,2,4,2};
+
+        // Para el array sin numeros iniciales, esta es la forma de agregar datos al array
         numeros[1] = 10;
         numeros[0]= 1000;
         numeros[9] = 200;
+
+        // Esta es la forma de remplazar un array que ya tiene datos iniciales
+        numbers [0]= 100;
+        numbers [1] = 200;
+        numbers [2] = 300;
+
+        // Se puede ver los valores iniciales del array
         System.out.println(Arrays.toString(numeros));
+        System.out.println(Arrays.toString(numbers));
+
+        // Indica la cantidad de elementos dentro del array
         System.out.println(numeros.length);
+        System.out.println(numbers.length);
+
+        // Hace Que los numeros se ordenen de nemor a mayor
         Arrays.sort(numeros);
-        System.out.println(Arrays.toString(numeros)); // Ordena el array de menor a mayor
+        Arrays.sort(numbers);
+        System.out.println(Arrays.toString(numeros));
+        System.out.println(Arrays.toString(numbers));
+
+
 
 
     }
---
+++ Java/src/arrays_multiples_dimensiones.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class arrays_multiples_dimensiones{
+    public static void main(String[] args) {
+        int[] numeros= {23,43,23,53,};
+        System.out.println("This is the form of one array or list simple");
+        System.out.println(Arrays.toString(numeros));
+
+        // This is one array or list witch more than one array or list
+        int [][] matrixNumbers= {{32,32,12},{43,24,23}};
+        System.out.println("This is the form of one array composed of two rows");
+        // Form of print the numbers of the matrix
+        System.out.println(Arrays.deepToString(matrixNumbers)); // Serves for print numbers of the matrix
+
+
+        //Se crea la estructura de la matriz sin numeros
+        int [][] matrizNothingNumbers= new int [3][3];
+        // Form of add numbers to the matrix
+        matrizNothingNumbers[0][0]=1;
+        matrizNothingNumbers[0][1]=1;
+        matrizNothingNumbers[0][2]=2;
+        // Form of print the numbers of the matrix
+        System.out.println(Arrays.deepToString(matrizNothingNumbers));
+
+        // Now creating one array of three dimensions
+        int [] [] [] threeDimentionsArray = new int [3][3][3];
+        threeDimentionsArray [0][0][0] = 100;
+        System.out.println(Arrays.deepToString(threeDimentionsArray));
+    }
+}
