2c9c6420293b9812ba16d07e4b731b74bfef1d76
KevinTimaran <kevin.buesaquillo@campusucc.edu.co>
2026-03-06 11:53:39 -0500

    06/03/2026

---
+++ Java/src/Ejercicios/primera_APP_Terminal_loop.java
@@ -0,0 +1,23 @@
+package Ejercicios;
+
+import java.util.Scanner;
+
+public class primera_APP_Terminal_loop {
+    public static void main(String[] args) {
+
+        Scanner scaner = new Scanner(System.in);
+        String  imput = "";
+
+        // If the word "Exit" is not entered, the loop will not close.
+        while (!imput.equals("exit")){
+
+            System.out.println("enter command");
+            imput = scaner.next().toLowerCase(); // toLowerCase es para que interprete mayusculas y minusculas
+            System.out.println(imput);
+        }
+
+        System.out.println("quit");
+
+
+    }
+}
---
+++ Java/src/Teoria/bucles.java
@@ -0,0 +1,29 @@
+package Teoria;
+// For
+// While
+// Do While
+
+public class bucles {
+    public  static void main(String[] args) {
+        //-----------------------------------------------------
+        //This is the loop for
+        //-----------------------------------------------------
+        String [] texto = {"Hello word", "User", "User one"};
+
+        for (int i = 0; i < 5; i++){
+            System.out.println("Hello word " + i);
+        };
+
+
+        //-----------------------------------------------------
+        //This is the loop whale
+        //-----------------------------------------------------
+        int p = 0;
+        while (p < 5){
+            System.out.println("Hello word " + p);
+            p++;
+        };
+
+
+    }
+}
