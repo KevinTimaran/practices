03181bc6d769f7671d3b15fe83093644bb33f961
KevinTimaran <kevin.buesaquillo@campusucc.edu.co>
2026-03-09 17:06:39 -0500

    get and set

--- Java/src/Teoria/Curso_Programacion_POO/Teoria/Class/Class_Constructor.java
+++ Java/src/Teoria/Curso_Programacion_POO/Teoria/Class/Class_Constructor.java
@@ -1,9 +1,10 @@
 package Teoria.Curso_Programacion_POO.Teoria.Class;
 // Class
 // Attributes
-// Method
 // Method constructor
 // Get and set: These methods come after the constructor method
+//We're going to use the set method
+// Method
 public class Class_Constructor {
         // This is the attributes, this datas are  globals
         int id;
@@ -16,12 +17,37 @@ public class Class_Constructor {
         this.name = name;
         this.surname = surname;
     }
+    // 2. ¡AGREGA ESTO! El constructor vacío
+    public Class_Constructor() {
+        // Al dejarlo vacío, permites la creación de un objeto "en blanco"
+    }
 
+    // Methods get and set
+    // Get = traeme / Brindame: Voy a poder optener los datos para mostrar los datos o hacer operaciones
+    // Set = Colocame: Colocar o estableser valores a los atributos este no retorna nada
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public String getSurname() {
+        return surname;
+    }
 
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
 
     //This is the methods
         public void mostrarNombre(){
--- Java/src/Teoria/Curso_Programacion_POO/Teoria/Class/Mian.java
+++ Java/src/Teoria/Curso_Programacion_POO/Teoria/Class/Mian.java
@@ -2,6 +2,28 @@ package Teoria.Curso_Programacion_POO.Teoria.Class;
 
 public class Mian {
     public static void main(String[] args) {
-        Class_Constructor student1= new Class_Constructor(32, "Kevin", "Buesaquillo");
+        Class_Constructor student1= new Class_Constructor(3, "Kevin", "Buesaquillo");
+        Class_Constructor student2= new Class_Constructor();
+
+        System.out.println("The ID of the student is " + student1.getId());
+        System.out.println("The name of the student is " + student1.getName());
+        System.out.println("The surname of the student is " + student1.getSurname());
+
+        student2.setId(2);
+        student2.setName("David");
+        student2.setSurname("Santiago");
+
+        System.out.println("----------------------------------------------------------");
+        System.out.println("The ID of the student is " + student2.getId());
+        System.out.println("The name of the student is " + student2.getName());
+        System.out.println("The surname of the student is " + student2.getSurname());
+
+        System.out.println("-----------------------------------------------------------");
+        //We're going to use the set method
+        student1.setId(433);
+        System.out.println("The ID of the student is " + student1.getId());
+        System.out.println("The name of the student is " + student1.getName());
+        System.out.println("The surname of the student is " + student1.getSurname());
+
     }
 }
