882294378f8a1cd8730e3198074a57c7585bd636
KevinTimaran <kevin.buesaquillo@campusucc.edu.co>
2026-03-10 17:58:31 -0500

    10/03/2026

---
+++ JavaScript/Teoria/02-operadores/01-aritmeticos.js
@@ -0,0 +1,19 @@
+let a = 5,
+    b = 3;
+
+// console.log(a + b); // 8
+// console.log(a - b); // 2
+// console.log(a * b); // 15
+// console.log(a / b); // 1.6666666666666667
+// console.log(a % b); // 2
+// console.log(a ** b); // 125 
+
+// Incremento 
+console.log(++a); // 6
+console.log(a++); // 6
+console.log(a); // 7
+
+// Decremento
+console.log(--b); // 2
+console.log(b--); // 2
+console.log(b); // 1    
