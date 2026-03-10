632200ba9c2683207082aa06faa71114ac8e16c5
KevinTimaran <kevin.buesaquillo@campusucc.edu.co>
2026-03-10 11:30:47 -0500

    funciones en JS

---
+++ JavaScript/Teoria/01-tipos/08-funciones.js
@@ -0,0 +1,17 @@
+function saludar(nombre){
+    console.log('Hola mundo');
+}
+
+saludar();
+
+function sumar(a, b){
+    return a + b;
+}
+
+console.log(sumar(2, 3)); // 5
+
+function multiplicar(){
+    return 2 * 3;
+}
+let resultado = multiplicar();
+console.log(resultado); // 6
