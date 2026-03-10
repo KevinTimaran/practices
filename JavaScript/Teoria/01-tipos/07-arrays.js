9c4903e538c400822a3931d4fe8f22077a7f0b83
KevinTimaran <kevin.buesaquillo@campusucc.edu.co>
2026-03-10 10:24:46 -0500

    arrays en JS

---
+++ JavaScript/Teoria/01-tipos/07-arrays.js
@@ -0,0 +1,6 @@
+let animales = ["Perro", "Gato", "Pájaro"]; // Array de strings
+console.log(animales);
+console.log(animales[0]); // Imprime: Perro
+
+animales[3]= "Pez"; // Agrega un nuevo elemento al array
+console.log(animales); // Imprime: ["Perro", "Gato", "Pájaro", "Pez"]
