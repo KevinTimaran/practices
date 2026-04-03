e5466859b540ec580b548be6aaad40e543ffdb4b
KevinTimaran <kevin.buesaquillo@campusucc.edu.co>
2026-04-02 21:06:30 -0500

    FizzBuzz

---
+++ Java/src/Ejercicios/Ejercicios_ MoureDev/RetosLogica/src/ModulosLogica/FizzBuzz.java
@@ -0,0 +1,28 @@
+package ModulosLogica;
+
+public class FizzBuzz {
+    /*
+     * Escribe un programa que muestre por consola (con un print) los
+     * números de 1 a 100 (ambos incluidos y con un salto de línea entre
+     * cada impresión), sustituyendo los siguientes:
+     * - Múltiplos de 3 por la palabra "fizz".
+     * - Múltiplos de 5 por la palabra "buzz".
+     * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+     */
+    public static void main(String[] args) {
+        for (int i = 1; i< 101; i++){
+            System.out.println();
+            int isFizz = i % 3;
+            int isBuzz = i % 5;
+
+            if (isFizz == 0 && isBuzz ==0) {
+                System.out.println("fizzbuzz");
+            }else if (isFizz == 0) {
+                System.out.println("fizz");
+            }else if (isBuzz == 0) {
+                System.out.println("buzz");
+            } else System.out.println(i);
+        };
+
+    }
+}
