d94a5e2a19f39a2263741d60f14326ab019b8469
Kevin Timaran <kevin.buesaquillo@campusucc.edu.co>
2026-08-11 18:33:34 -0500

    feat: i comment the module FizzBuzz

--- Java/src/Ejercicios/Ejercicios_ MoureDev/RetosLogica/src/ModulosLogica/FizzBuzz.java
+++ Java/src/Ejercicios/Ejercicios_ MoureDev/RetosLogica/src/ModulosLogica/FizzBuzz.java
@@ -9,12 +9,23 @@ public class FizzBuzz {
      * - Múltiplos de 5 por la palabra "buzz".
      * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
      */
+
+
+    // First: Defined a class or method main
     public static void main(String[] args) {
+         // Second: i integrate a function for. This function have a
+        // var i, the var i start which the number one and this var have a limit
+        // of one hundred one.
         for (int i = 1; i< 101; i++){
+
+            // This print the number of loop for
             System.out.println();
+
+            // This part calculated the residue of number
             int isFizz = i % 3;
             int isBuzz = i % 5;
 
+            // Are condicions
             if (isFizz == 0 && isBuzz ==0) {
                 System.out.println("fizzbuzz");
             }else if (isFizz == 0) {
