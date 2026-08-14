28fab4723166cc970faa4a4c9fa420e6434d630e
Kevin Timaran <kevin.buesaquillo@campusucc.edu.co>
2026-08-13 23:54:10 -0500

    feat: i Complet the module Anagrama

--- Java/src/Ejercicios/Ejercicios_ MoureDev/RetosLogica/src/ModulosLogica/Anagrama.java
+++ Java/src/Ejercicios/Ejercicios_ MoureDev/RetosLogica/src/ModulosLogica/Anagrama.java
@@ -13,6 +13,35 @@ import java.util.Scanner;
 
 public class Anagrama {
     public static void main (String[] args){
+        System.out.println("Enter the one word");
+        Scanner askOneWord = new Scanner(System.in);
+        String oneWord = askOneWord.nextLine();
+
+
+        System.out.println("Enter the two word");
+        Scanner askTwoWord = new Scanner(System.in);
+        String twoWord = askTwoWord.nextLine();
+
+        char [] listOneWord = oneWord.toCharArray();
+        char [] listTwoWord = twoWord.toCharArray();
+
+        if (listOneWord.length > 1  ){
+            int palabra = listOneWord.length;
+
+            System.out.println("La palabra"+ askOneWord + "si es una anagrama debido \n" +
+                    "a que esta tiene: " + palabra + "letras" );
+
+            for (int i = 0; i < listOneWord.length;  i++ ){
+                System.out.println("Letra"+i+":"+listOneWord[i]);}
+
+        } else {
+            int palabra = listOneWord.length;
+            System.out.println("la palabra no es un anagrama debido a que tiene"+palabra+"letras");
+
+        }
+
+
+        /**
 
 
         // Aks
@@ -24,13 +53,20 @@ public class Anagrama {
         Scanner aksLastWord = new Scanner(System.in);
         String lastWord = aksLastWord.nextLine();
 
-        //Convert a string text to characters list
+        //Convert a string text to characters list. toCharArray() return a characters list
+        // This function separate the word in letters
         char[] firtsLetters = firtsWord.toCharArray();
         char[] lastLetters = lastWord.toCharArray();
 
 
+
+        // The proprietary "length" count how many letters have the word
         if (firtsLetters.length > 1){
+
+            // Save the number letters in a var
             int letras = firtsLetters.length;
+
+            //Print the result
             System.out.println("VERDADER \nLa palabra: "+ firtsWord +
                     " \nes un anagrama de: " + letras+ " Letras");
 
@@ -55,7 +91,7 @@ public class Anagrama {
             }
         } else {
             int letras = lastLetters.length;
-            System.out.println("FALSO. \nEsta segunda palabra solo contiene "+ letras+ " letras");}
+            System.out.println("FALSO. \nEsta segunda palabra solo contiene "+ letras+ " letras");}**/
 
     }
 }
