15a8d89017e4daf3d9d8b0a7478084dcf1bd12bd
Kevin Timaran <kevin.buesaquillo@campusucc.edu.co>
2026-08-21 18:42:13 -0500

    feat: i completed Fibonacci

--- Java/src/Ejercicios/Ejercicios_ MoureDev/RetosLogica/src/ModulosLogica/Fibonacci.java
+++ Java/src/Ejercicios/Ejercicios_ MoureDev/RetosLogica/src/ModulosLogica/Fibonacci.java
@@ -2,45 +2,46 @@ package ModulosLogica;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Fibonacci {
 
+
     public static void main (String[] args){
+        /*
+         * Escribe un programa que imprima los 50 primeros números de la sucesión
+         * de Fibonacci empezando en 0.
+         * - La serie Fibonacci se compone por una sucesión de números en
+         *   la que el siguiente siempre es la suma de los dos anteriores.
+         *   0, 1, 1, 2, 3, 5, 8, 13...
+         */
 
+        List<Long> num = new ArrayList<>();
 
+        long a = 0;
+        long b = 1;
 
-        List<Integer> numero = new ArrayList<>();
+        num.add(a);
+        num.add(b);
 
+        System.out.println("0:"+num.get(0));
+        System.out.println("1:"+num.get(1));
 
-        if ( numero.get(50) < 51){
+        for (int i = 2; i < 51; i++ ){
 
-            numero.add(0);
-            System.out.println(numero.get(0));
+            int firstNum = i-2;
+            int secondNum = i-1;
 
+            long saveNum = num.get(firstNum) + num.get(secondNum);
 
-            numero.add(1);
-            System.out.println(numero.get(1));
+            System.out.println(
+                    i +":"+ saveNum
+            );
 
-            for (int i = 0; i < 51;)
-
-                System.out.println(numero.get(1)+ numero.get(-i) );
-
-
-
-
-
-
-
-
-
-
-
+            num.add(saveNum);
         }
-
-
+        System.out.println(num);
 
 
     }
-    
-    
 }
