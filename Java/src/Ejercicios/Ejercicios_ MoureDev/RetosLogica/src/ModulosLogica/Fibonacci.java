1bb1cf0727b69fd215b776ed68a48d7e3f9f1b85
Kevin Timarán <kevin.buesaquillo@campusucc.edu.co>
2026-08-21 18:08:50 -0500

    fear: Se creo moudulo numeroPrimo
    # Conflicts:
    #	Java/src/Ejercicios/Ejercicios_ MoureDev/RetosLogica/src/ModulosLogica/Fibonacci.java

--- Java/src/Ejercicios/Ejercicios_ MoureDev/RetosLogica/src/ModulosLogica/Fibonacci.java
+++ Java/src/Ejercicios/Ejercicios_ MoureDev/RetosLogica/src/ModulosLogica/Fibonacci.java
@@ -2,46 +2,45 @@ package ModulosLogica;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Fibonacci {
 
-
     public static void main (String[] args){
-        /*
-         * Escribe un programa que imprima los 50 primeros números de la sucesión
-         * de Fibonacci empezando en 0.
-         * - La serie Fibonacci se compone por una sucesión de números en
-         *   la que el siguiente siempre es la suma de los dos anteriores.
-         *   0, 1, 1, 2, 3, 5, 8, 13...
-         */
 
-        List<Long> num = new ArrayList<>();
 
-        long a = 0;
-        long b = 1;
 
-        num.add(a);
-        num.add(b);
+        List<Integer> numero = new ArrayList<>();
 
-        System.out.println("0:"+num.get(0));
-        System.out.println("1:"+num.get(1));
 
-        for (int i = 2; i < 51; i++ ){
+        if ( numero.get(50) < 51){
 
-            int firstNum = i-2;
-            int secondNum = i-1;
+            numero.add(0);
+            System.out.println(numero.get(0));
 
-            long saveNum = num.get(firstNum) + num.get(secondNum);
 
-            System.out.println(
-                    i +":"+ saveNum
-            );
+            numero.add(1);
+            System.out.println(numero.get(1));
 
-            num.add(saveNum);
+            for (int i = 0; i < 51;)
+
+                System.out.println(numero.get(1)+ numero.get(-i) );
+
+
+
+
+
+
+
+
+
+
+
         }
-        System.out.println(num);
+
+
 
 
     }
+    
+    
 }
---
+++ Java/src/Ejercicios/Ejercicios_ MoureDev/RetosLogica/src/ModulosLogica/numeroPrimo.java
@@ -0,0 +1,32 @@
+package ModulosLogica;
+
+public class numeroPrimo {
+    public static void main (String[] args){
+        /*
+         * Escribe un programa que se encargue de comprobar si un número es o no primo.
+         * Hecho esto, imprime los números primos entre 1 y 100.
+         */
+
+
+        for (int i = 0; i < 101; i++){
+
+            int num = i % 2;
+
+            if (num == 0);
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+    }
+
+}
